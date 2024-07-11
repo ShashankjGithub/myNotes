c9de1f7a0df763703540a14c086aff363d1a5381
ShashankjGithub <shashankshah042@gmail.com>
2024-07-11 09:55:22 +0530

    add login page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login';
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from './components/Home';
 
 function App() {
   return (
     <>
-     <Login/>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Login to = "/login" replace={true}/>} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/home" element={<Home />} />
+      </Routes>
+
+    </Router>
+ 
       </>
     
   );
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,51 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Loginn, { Context, Password, Submit, Username } from '@react-login-page/page7';
+import {useNavigate} from 'react-router-dom';
+
 const background = require('../bg.jpg')
-function login() {
+
+
+
+
+export default function Login() {
+  let navigate = useNavigate();
+  const [username, setUserName] = useState()
+  const [password, setPassword] = useState()
+  
+  const UserLogin = () =>{
+   console.log(username)
+   console.log(password)
+   if(username === "admin" && password === "admin"){
+   
+     navigate('/home')
+   }
+   // eslint-disable-next-line no-lone-blocks
+   
+  }
+  const setUserNameText = (event)=>{
+    setUserName(event.target.value)
+  }
+
+  const setUserPasswordText = (event)=>{
+    setPassword(event.target.value)
+  }
+
+
   return (
     <div>
-        <div style={{
-            backgroundImage : `url(${background})`,
-            backgroundPosition: 'center',
-            backgroundSize: 'cover',
-            backgroundRepeat: 'no-repeat',
-            height: "100vh",
-          
-        }} > Hello </div>
+      <Loginn style={{height : "100vh"}}>
+        <Username value = {username} onChange={setUserNameText}>
+         
+        </Username>
+        <Password value={password} onChange={setUserPasswordText}>
+
+        </Password>
+      <Submit name='Login' onClick={UserLogin}>
+       Login
+      </Submit>      
+      </Loginn>
+      
     </div>
   )
 }
 
-export default login
